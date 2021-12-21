e1e820402283afb1425be118a5481b2b804c72bd
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-12-21 23:19:26 +0900

    基本のテスト環境作成

---
+++ vitest/src/add.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from "vitest";
+import { add } from "./add";
+
+describe("add", () => {
+  it("1 + 2 = 3", () => {
+    const result = add(1, 2);
+
+    expect(result).toBe(3);
+  });
+});
